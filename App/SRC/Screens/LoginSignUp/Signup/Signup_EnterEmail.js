6ee22acea2df794a68e2a0172db56a3b6551e119
Pankaj Kumar Meena <prpmtp2001@gmail.com>
2022-10-17 22:48:23 +0530

    "User registration and Email-veification"

--- App/SRC/Screens/LoginSignUp/Signup/Signup_EnterEmail.js
+++ App/SRC/Screens/LoginSignUp/Signup/Signup_EnterEmail.js
@@ -1,38 +1,83 @@
-import { StyleSheet, Text,TouchableOpacity, View ,Image, TextInput} from 'react-native'
-import React from 'react'
-import { goback,containerFull, logo1 } from '../../../CommonCss/pagecss'
-import { Ionicons } from '@expo/vector-icons';
-import logo from '../../../CommonCss/pagecss'
-import { formbtn,formHead2,formInput } from '../../../CommonCss/formcss';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, ActivityIndicator } from 'react-native'
+import React, { useState } from 'react'
+import { containerFull, goback, hr80, logo1 } from '../../../CommonCss/pagecss'
+import logo from '../../../../assets/logo.png'
+import { formbtn, formHead, formHead2, formInput, formTextLinkCenter, formTextLinkRight } from '../../../CommonCss/formcss'
+import { MaterialIcons } from '@expo/vector-icons';
+const Signup_EnterEmail = ({ navigation }) => {
+    const [email, setEmail] = useState('')
+    const [loading, setLoading] = useState(false)
+    const handleEmail = () => {
+        // setLoading(true)
+        // navigation.navigate('Signup_EnterVerificationCode')
+        if (email == '') {
+            alert('Please enter email')
+        }
+        else {
+            setLoading(true)
+            fetch('http://10.0.2.2:3000/verify', {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: email
+                })
+            })
+                .then(res => res.json()).then(
+                    data => {
+                        if (data.error === "Invalid Credentials") {
+                            // alert('Invalid Credentials')
+                            alert('Invalid Credentials')
+                            setLoading(false)
+                        }
+                        else if (data.message === "Verification Code Sent to your Email") {
+                            setLoading(false)
+                            alert(data.message);
+                            navigation.navigate('Signup_EnterVerificationCode', {
+                                useremail: data.email,
+                                userVerificationCode: data.VerificationCode
+                            })
 
-const Signup_EnterEmail = ({navigation}) => {
-  return (
-    <View style={containerFull}>
+                        }
+                    }
+                )
+        }
+    }
+    return (
+        <View style={containerFull}>
+            <TouchableOpacity onPress={() => navigation.navigate('Login')} style={goback}>
 
-      <TouchableOpacity onPress={() => navigation.navigate("Login")} style={goback}>
-      <Ionicons name="arrow-back" size={28} color="gray" />
-        <Text style={{
-          color:'gray',
-          fontSize:16,
-          marginLeft:5,
-          fontWeight: 'bold',
-        }}>
-           Go Back
-          </Text>
-      </TouchableOpacity>
+                <MaterialIcons name="arrow-back-ios" size={24} color="gray" />
+                <Text style={{
+                    color: 'gray',
+                    fontSize: 16,
+                }}
 
-        <Image source={logo} style={logo1} />
-        <Text style= {formHead2}>Create a new account</Text>
-        <TextInput placeholder="Enter Your Email" style={formInput} />
-       
-        <Text style={formbtn}
-                onPress={() => navigation.navigate('Signup_EnterVerificationCode')}
-            >
-                Next
-        </Text>
-    
-    </View>
-  )
+                >Go Back</Text>
+
+            </TouchableOpacity>
+
+            <Image source={logo} style={logo1} />
+            <Text style={formHead2}>Create a new account</Text>
+            <TextInput placeholder="Enter Your Email" style={formInput}
+
+                onChangeText={(text) => {
+                    setEmail(text)
+                }}
+            />
+            {
+                loading ?
+                    <ActivityIndicator size="large" color="white" />
+                    :
+                    <Text style={formbtn}
+                        onPress={() => handleEmail()}
+                    >
+                        Next
+                    </Text>
+            }
+        </View>
+    )
 }
 
 export default Signup_EnterEmail
--- AppBackend/routes/authRoutes.js
+++ AppBackend/routes/authRoutes.js
@@ -5,6 +5,7 @@ const User = mongoose.model("User");
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const nodemailer = require("nodemailer");
+const bcrypt = require("bcrypt");
 
 
 // router.get('/home', (req, res) => {
@@ -28,7 +29,7 @@ async function mailer(recieveremail, code) {
 
 
     let info = await transporter.sendMail({
-        from: "PankajMeena",
+        from: "GeekChat",
         to: `${recieveremail}`,
         subject: "Email Verification",
         text: `Your Verification Code is ${code}`,
@@ -40,32 +41,168 @@ async function mailer(recieveremail, code) {
 }
 
 router.post('/verify', (req, res) => {
-    // console.log(req.body);
+    console.log('sent by client', req.body);
     const { email } = req.body;
 
     if (!email) {
         return res.status(422).json({ error: "Please add all the fields" });
     }
+
+    User.findOne({ email: email }).then(async (savedUser) => {
+        if (savedUser) {
+            return res.status(422).json({ error: "Invalid Credentials" });
+        }
+        try {
+            let VerificationCode = Math.floor(100000 + Math.random() * 900000);
+            await mailer(email, VerificationCode);
+            console.log("Verification Code", VerificationCode);
+            res.send({ message: "Verification Code Sent to your Email", VerificationCode, email });
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+    )
+})
+
+
+router.post('/changeusername', (req, res) => {
+    const { username, email } = req.body;
+
+    User.find({ username }).then(async (savedUser) => {
+        if (savedUser.length > 0) {
+            return res.status(422).json({ error: "Username already exists" });
+        }
+        else {
+            return res.status(200).json({ message: "Username Available", username, email });
+        }
+    })
+})
+
+router.post('/signup', async (req, res) => {
+    const { username, password, email } = req.body;
+    if (!username || !password || !email) {
+        return res.status(422).json({ error: "Please add all the fields" });
+    }
+    else {
+        const user = new User({
+            username,
+            email,
+            password,
+        })
+
+        try {
+            await user.save();
+            const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+            return res.status(200).json({ message: "User Registered Successfully", token });
+
+        }
+        catch (err) {
+            console.log(err);
+            return res.status(422).json({ error: "User Not Registered" });
+        }
+    }
+})
+
+
+
+// forgot password
+
+router.post('/verifyfp', (req, res) => {
+    console.log('sent by client', req.body);
+    const { email } = req.body;
+
+    if (!email) {
+        return res.status(422).json({ error: "Please add all the fields" });
+    }
+
+    User.findOne({ email: email }).then(async (savedUser) => {
+        if (savedUser) {
+            try {
+                let VerificationCode = Math.floor(100000 + Math.random() * 900000);
+                await mailer(email, VerificationCode);
+                console.log("Verification Code", VerificationCode);
+                res.send({ message: "Verification Code Sent to your Email", VerificationCode, email });
+            }
+            catch (err) {
+                console.log(err);
+            }
+        }
+        else {
+            return res.status(422).json({ error: "Invalid Credentials" });
+        }
+    }
+    )
+})
+
+
+router.post('/resetpassword', (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(422).json({ error: "Please add all the fields" });
+    }
     else {
         User.findOne({ email: email })
             .then(async (savedUser) => {
-                // console.log(savedUser);
-                // return res.status(200).json({ message: "Email sent" });
                 if (savedUser) {
+                    savedUser.password = password;
+                    savedUser.save()
+                        .then(user => {
+                            res.json({ message: "Password Changed Successfully" });
+                        })
+                        .catch(err => {
+                            console.log(err);
+                        })
+                }
+                else {
                     return res.status(422).json({ error: "Invalid Credentials" });
                 }
-                try {
-                    let VerificationCode = Math.floor(100000 + Math.random() * 900000);
-                    await mailer(email, VerificationCode);
+            })
+    }
 
-                    return res.status(200).json({ message: "Email sent", VerificationCode, email });
+})
+
+router.post('/signin', (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(422).json({ error: "Please add all the fields" });
+    }
+    else {
+        User.findOne({ email: email })
+            .then(savedUser => {
+                if (!savedUser) {
+                    return res.status(422).json({ error: "Invalid Credentials" });
                 }
-                catch (err) {
-                    return res.status(422).json({ error: "Error sending email" });
+                else {
+                    console.log(savedUser);
+                    bcrypt.compare(password, savedUser.password)
+                        .then(
+                            doMatch => {
+                                if (doMatch) {
+                                    const token = jwt.sign({ _id: savedUser._id }, process.env.JWT_SECRET);
+
+                                    const { _id, username, email } = savedUser;
+
+                                    res.json({ message: "Successfully Signed In", token, user: { _id, username, email } });
+                                }
+                                else {
+                                    return res.status(422).json({ error: "Invalid Credentials" });
+                                }
+                            }
+                        )
+                    // res.status(200).json({ message: "User Logged In Successfully", savedUser });
                 }
             })
-        // return res.status(200).json({ message: "Email sent" });
+            .catch(err => {
+                console.log(err);
+            })
     }
 })
 
 module.exports = router;
+
+
+// $2b$08$1Hkn50MGDJSrwBt024DySerDtuFrDzFIDY8bkev83PS7RWC3m67lC
+// $2b$08$znPHy0v.QmmESyqTruTXuu2DER6Y5wmXk6Y/W9sIZw4bRqgsZpGyS
